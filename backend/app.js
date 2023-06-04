648e6995e60a0b262b14a1a14ac3c6978441cf8e
Deepa1401 <109983614+Deepa1401@users.noreply.github.com>
2023-06-04 16:13:02 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use("/api/user",router);
 app.use("/api/blog", blogRouter);
 
-mongoose.connect('mongodb+srv://Deepu:faUfBe47L6OuJ99o@cluster0.oemg5m6.mongodb.net/?retryWrites=true&w=majority')
+mongoose.connect('mongodb+srv://Deepu:HN3IsjIzLNOQxxT3@cluster0.oemg5m6.mongodb.net/')
 .then(()=>app.listen(7000))
 .then(()=>console.log('connected to database and listening to localhost 7000'))
 .catch((err)=>console.log(err));
